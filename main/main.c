c3068cf190ae0b3803029d6e71d5495db4807465
C47D <carlos.santiago.diaz@gmail.com>
2020-04-11 23:07:39 -0500

    Add sd card code to main

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
-/* LVGL Example project
+/* Shared SPI for the M5Stack
  * 
- * Basic project to test LVGL on ESP32 based projects.
+ * The code halts when sharing the spi bus between the m5 display and the
+ * sd card adapter.
  *
  * This example code is in the Public Domain (or CC0 licensed, at your option.)
  *
@@ -8,6 +9,7 @@
  * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied.
  */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,36 +23,221 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <sys/dirent.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "sdmmc_cmd.h"
+
+#define SD_CARD_MOSI	23
+#define SD_CARD_MISO	19
+#define SD_CARD_CLK	18
+#define SD_CARD_CS	4
+
+#define DMA_CHANNEL	2
+#define MAX_BUFSIZE	(16 * 1024)
+
+void test_sd_card(void);
+
 /* Littlevgl specific */
-#include "lvgl.h"
+#include "lvgl/lvgl.h"
 #include "lvgl_driver.h"
 #include "lv_examples/lv_apps/demo/demo.h"
 
-/*********************
- *      DEFINES
- *********************/
+static const char *TAG = "SD-CARD";
 
-/**********************
- *  STATIC PROTOTYPES
- **********************/
 static void IRAM_ATTR lv_tick_task(void *arg);
 void guiTask();
 
-
-/**********************
- *   APPLICATION MAIN
- **********************/
 void app_main() {
-    
-    //If you want to use a task to create the graphic, you NEED to create a Pinned task
-    //Otherwise there can be problem such as memory corruption and so on
+    /* lvgl task */
     xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+
+    printf("Screen is working...\n");
+    printf("Writing to the card in...\n");
+
+    for (size_t i = 10; i > 0; i--) {
+	printf("..%d\n", i);
+	vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    printf("Writing to card\n");
+    test_sd_card();
+    printf("SD card routine complete\n");
+    printf("Screen is now frozen\n");
+}
+
+/* Mostly from test/test_sdio */
+void test_sd_card(void)
+{
+#if 0 /* Setting the spi bus, not needed, we do it already on lvgl */
+    spi_bus_config_t bus_cfg = {
+	.mosi_io_num = SD_CARD_MOSI,
+	.miso_io_num = SD_CARD_MISO,
+	.sclk_io_num = SD_CARD_CLK,
+	.quadwp_io_num = -1,
+	.quadhd_io_num = -1,
+	.max_transfer_sz = 4000,
+    };
+    esp_err_t spi_bus_ret = spi_bus_initialize(host.slot, &bus_cfg, SPI_DMA_CHAN);
+    if (ESP_OK != spi_bus_ret) {
+	ESP_LOGE(TAG, "Failed to initialize bus.");
+	return;
+    }
+#endif
+
+    sdspi_device_config_t device_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    device_config.host_id = /* Same as the display */;
+    device_config.gpio_cs = SD_CARD_CS;
+    device_config.gpio_int = SDSPI_SLOT_NO_INT /* Interrupt line */;
+    device_config.gpio_cd = SDSPI_SLOT_NO_CD /* Card detect line */;
+    device_config.gpio_wp = SDSPI_SLOT_NO_WP /* Write protect line  */;
+
+    esp_err_t int_err = gpio_install_isr_service(0);
+    assert(ESP_OK == int_err);
+
+#if 0
+    /* Extra configuration for SPI host
+     * NOTE: SPI Host already configured, so we don't use this */
+    sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+    slot_config.gpio_miso = SD_CARD_MISO;
+    slot_config.gpio_mosi = SD_CARD_MOSI;
+    slot_config.gpio_sck = SD_CARD_CLK;
+    slot_config.gpio_cs = SD_CARD_CS;
+    slot_config.dma_channel = DMA_CHANNEL;
+#endif
+
+    /* Init the SD SPI device and attach to its bus
+     * 
+     * NOTE: Init the spi bus by spi_bus_initialize before calling this
+     * function. */
+
+    sdspi_dev_handle_t sdspi_handle;
+    /* XXX: Check we really need this */
+    sdspi_host_init();
+
+    /* TODO: Change slot config by device_config */
+    esp_err_t sdspi_host_err = sdspi_host_init_device(&device_config,
+	&sdspi_handle
+    );
+    assert(sdspi_host_err == ESP_OK);
+
+    /* Store the SD SPI device state and configurations of
+     * upper layer (SD/SDIO/MMC driver)
+     *
+     * Modify the slot parameter of the structure to the SD SPI
+     * device handle just returned from sdspi_host_init_device.
+     * Call sdmmc_card_init with the sdmmc_host_t to probe and init
+     * the SD Card. */
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    host.slot = sdspi_handle;
+
+    /* Mount configuration */
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+	.format_if_mount_failed = false,
+	.max_files = 5,
+	.allocation_unit_size = MAX_BUFSIZE,
+    };
+
+    /* Use configuration and mount the file system */
+    sdmmc_card_t *card = NULL;
+    
+    /* Wait for at least 5 seconds */
+    int retry_times = 5;
+    do {
+	if (ESP_OK == sdmmc_card_init(&config, card)) {
+	    break;
+	}
+
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+    } while (--retry_times);
+    
+    
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard",
+	&host,		// Host configuration
+	&slot_config,	// Slot configuration
+	&mount_config,	// Mount configuration
+	&card		// out card
+    );
+
+    if (ESP_OK != ret) {
+	if (ESP_FAIL == ret) {
+	    ESP_LOGE(TAG, "Failed to mount filesystem. "
+		"If you want the card to be formatted, set format_if_mount_failed = true".);
+	} else {
+	    ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+		"Make sure SD card lines have pull-up resistors in place",
+		esp_err_to_name(ret));
+	}
+    }
+
+    sdmmc_card_print_info(stdout, card);
+
+    ESP_LOGI(TAG, "Opening file");
+
+    FILE *f = fopen("/sdcard/hello.txt", "w");
+    if (NULL == f){
+	ESP_LOGE(TAG, "Failed to open file for writing");
+	return;
+    }
+
+    fprintf(f, "Hello %s!", card->cid.name);
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat("/sdcard/foo.txt", &st) == 0) {
+	// Delete it if exists
+	unlink("/sdcard/foo.txt");
+    }
+
+    // Rename original file
+    ESP_LOGI(TAG, "Renaming file");
+    if (rename("/sdcard/hello.txt", "/sdcard/foo.txt") != 0) {
+	ESP_LOGE(TAG, "Rename failed");
+	return;
+    }
+
+    // Open renamed file for reading
+    ESP_LOGI(TAG, "Reading file");
+    f = fopen("/sdcard/foo.txt", "r");
+    if (NULL == f) {
+	ESP_LOGE(TAG, "Failed to open file for reading");
+	return;
+    }
+
+    char line[64] = "";
+    fgets(line, sizeof line, f);
+    fclose(f);
+
+    // Strip new line
+    char *pos = strchr(line, '\n');
+    if (pos) {
+	*pos = '\0';
+    }
+
+    ESP_LOGI(TAG, "Read from file: %s", line);
+
+    // All done, unmount partition and disable SDMMC or SPI peripheral
+    esp_vfs_fat_sdmmc_unmount();
+    ESP_LOGI(TAG, "Card unmounted");
 }
 
 static void IRAM_ATTR lv_tick_task(void *arg) {
     (void) arg;
 
     lv_tick_inc(portTICK_RATE_MS);
+}
+
+void some_random_task(void)
+{
+    printf("lvgl task still ticks but screen freezes\n");
 }
 
 //Creates a semaphore to handle concurrent call to lvgl stuff
@@ -89,12 +276,15 @@ void guiTask() {
             /* name is optional, but may help identify the timer when debugging */
             .name = "periodic_gui"
     };
+
     esp_timer_handle_t periodic_timer;
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     //On ESP32 it's better to create a periodic task instead of esp_register_freertos_tick_hook
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 10*1000)); //10ms (expressed as microseconds)
 
     demo_create();
+
+    lv_task_create(some_random_task, 2000, LV_TASK_PRIO_LOWEST, NULL);
 
     while (1) {
         vTaskDelay(1);
